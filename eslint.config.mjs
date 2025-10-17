0dd6b0412cb5b8be2947ef99524d413764e34d36
Bryan <bryanchuquimarcacastillo@gmail.com>
2025-10-17 15:10:38 +0200

    Docker

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import { parse } from 'dotenv';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended', 'plugin:prettier/recommended'],
+    languageOptions: { globals: globals.node },
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      parserOptions: { ecmaVersion: 'latest' },
+      globals: { ...globals.es2023, ...globals.common },
+    },
+  },
+
+  js.configs.recommended,
+]);
